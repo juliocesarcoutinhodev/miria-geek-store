cab18af1ead60ed8e65dd7693eddc4765c1ea2ad
Julio Cesar Coutinho <juliocesar.coutinhodev@outlook.com>
2026-05-14 15:00:59 -0300

    Initial backend project

---
+++ miria-geek-api/src/main/java/br/com/miriageekstore/MiriaGeekApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.miriageekstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiriaGeekApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiriaGeekApiApplication.class, args);
+    }
+
+}
---
+++ miria-geek-api/src/test/java/br/com/miriageekstore/MiriaGeekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miriageekstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiriaGeekApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
